3e514f38ccd6f3192d036a0659201d120be3e8f4
Carolaine Machado <carolainemachado12345@gmail.com>
2023-12-29 20:16:16 -0300

    polimorfismo

---
+++ Polimorfismo/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+        import entities.Account;
+        import entities.BusinessAccount;
+        import entities.SavingsAccount;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        /*
+        Em Programação Orientada a Objetos, polimorfismo é recurso que
+        permite que variáveis de um mesmo tipo mais genérico possam
+        apontar para objetos de tipos específicos diferentes, tendo assim
+        comportamentos diferentes conforme cada tipo específico
+         */
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+    }
+}
